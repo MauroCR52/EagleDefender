62f15aceb256972c775810c733f0bfe83c1a2cb8
MauroCR52 <mauluna52@gmail.com>
2023-10-02 01:48:03 -0600

    validacion de inicio de sesion y crear un nuevo usuario

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import pygame
 import pygame_gui
@@ -11,29 +12,32 @@ class Initial_window:
         pygame.init()
         self.screen = pygame.display.set_mode((ancho, alto))
         pygame.display.set_caption("Login")
-        self.background = pygame.image.load("assets/fondo_espanol.png")
+        self.background = pygame.image.load("assets/login_bg.png")
 
         # Inicializar el administrador de interfaz de usuario de pygame_gui
-        self.gui_manager = pygame_gui.UIManager((ancho, alto))
+        self.gui_manager = pygame_gui.UIManager((ancho, alto), starting_language='es', translation_directory_paths=['config/translation'])
 
         # Crear un ComboBox
-        self.combo_box = pygame_gui.elements.UIDropDownMenu(options_list=["Español", "Ingles"],
-                                                            starting_option="Español",
-                                                            relative_rect=pygame.Rect(25, 50, 150, 50),
-                                                            manager=self.gui_manager)
+        languages_list = ['pygame-gui.English',
+                          'pygame-gui.Spanish']
+        self.combo_box = pygame_gui.elements.UIDropDownMenu(languages_list, 'pygame-gui.Spanish', relative_rect=pygame.Rect(25, 30, 150, 50), manager=self.gui_manager)
 
         #Cuadros de texto del Login
-        self.email_rect = pygame.Rect(920, 225, 350, 50)
-        self.password_rect = pygame.Rect(920, 320, 350, 50)
+        self.username_rect = pygame.Rect(920, 230, 350, 45)
+        self.password_rect = pygame.Rect(920, 320, 350, 45)
+        self.button_signin = pygame.Rect(920, 430, 150, 70)
+        self.button_signup = pygame.Rect(920, 520, 150, 70)
+
 
         # Crear un cuadro de entrada
-        self.email_entry = pygame_gui.elements.UITextEntryLine(relative_rect=self.email_rect,
-                                                        manager=self.gui_manager)
+        self.username_entry = pygame_gui.elements.UITextEntryLine(relative_rect=self.username_rect, manager=self.gui_manager)
+        self.password_entry = pygame_gui.elements.UITextEntryLine(relative_rect=self.password_rect, manager=self.gui_manager)
+        self.password_entry.set_text_hidden(True)
+
+        self.button_signin_label = pygame_gui.elements.UIButton(relative_rect=self.button_signin, text="language.sign_in", manager=self.gui_manager)
+        self.button_signup_label = pygame_gui.elements.UIButton(relative_rect=self.button_signup, text="language.sign_up", manager=self.gui_manager)
 
 
-
-
-       # self.button = Button((300, 200, 200, 50), (0, 0, 0, 0), text = "s")
 
 
     account_string = '''
@@ -58,7 +62,38 @@ class Initial_window:
                     pygame.quit()
                     sys.exit()
 
-              #  if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == self.button_signin:
+                        print("Hola")
+                        self.username_entry.clear()
+                        self.password_entry.clear()
+                    elif event.ui_element == self.button_signup:
+                        self.username_entry.get_text()
+                        self.password_entry.get_text()
+                        with open('config/users.json', 'r') as archivo:
+                            data = json.load(archivo)
+                        new_user = {
+                            "username": self.username_entry.get_text(),
+                            "password": self.password_entry.get_text(),
+                            "language": self.gui_manager.get_locale()
+                        }
+                        users = data["users"]
+                        user_exist = next((user for user in users if user["username"] == new_user["username"]), None)
+                        if user_exist:
+                            print(f"El usuario ya existe")
+                        else:
+                            data["users"].append(new_user)
+                            with open('config/users.json', 'w') as archivo:
+                                json.dump(data, archivo, indent=4)
+                            print("Usuario agregado")
+
+                if event.type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+                    if event.text == 'pygame-gui.English':
+                        self.gui_manager.set_locale('en')
+                    elif event.text == 'pygame-gui.Spanish':
+                        self.gui_manager.set_locale('es')
+
+                #  if event.type == pygame.MOUSEBUTTONDOWN:
                #     if self.ca_button.collidepoint(event.pos):
                #        print("Me cago en JSON")
 
@@ -70,10 +105,12 @@ class Initial_window:
 
                 # Pasar eventos de pygame a pygame_gui
                 self.gui_manager.process_events(event)            # Actualiza el administrador de interfaz de usuario de pygame_gui
+
+
+
+
             self.gui_manager.update(time_delta)
-
             self.screen.blit(self.background, (0, 0))
-
 
             # Dibuja los elementos de la interfaz de usuario de pygame_gui
             self.gui_manager.draw_ui(self.screen)
